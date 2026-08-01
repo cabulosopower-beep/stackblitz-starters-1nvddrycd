40cf6ca893170c98c48714f82d5ed079dcd19938
cabulosopower-beep <cabulosopower@gmail.com>
2026-08-01 12:40:44 -0300

    Alteração de busca de novas atualizações

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,88 @@
-const CACHE_NAME = "controle-financeiro-v1";
+const CACHE_NAME = "controle-financeiro-v2";
 
 const arquivos = [
     "./",
     "./index.html",
     "./manifest.json",
-  
-    "./styles.css",
-    "./script.js",
-  
     "./css/estilo.css",
     "./js/app.js",
-  
     "./icons/icon-192.png",
     "./icons/icon-512.png"
-  ];
+];
+
+
+// Instala o novo Service Worker
 
 self.addEventListener("install", function(event){
 
+    self.skipWaiting();
+
     event.waitUntil(
+
         caches.open(CACHE_NAME)
         .then(function(cache){
+
             return cache.addAll(arquivos);
+
         })
+
     );
 
 });
 
 
+// Remove caches antigos
+
+self.addEventListener("activate", function(event){
+
+    event.waitUntil(
+
+        caches.keys()
+        .then(function(keys){
+
+            return Promise.all(
+
+                keys.map(function(key){
+
+                    if(key !== CACHE_NAME){
+
+                        return caches.delete(key);
+
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+    self.clients.claim();
+
+});
+
+
+// Busca atualização primeiro
+
 self.addEventListener("fetch", function(event){
 
     event.respondWith(
-        caches.match(event.request)
+
+        fetch(event.request)
+
         .then(function(response){
 
-            return response || fetch(event.request);
+            return response;
 
         })
+
+        .catch(function(){
+
+            return caches.match(event.request);
+
+        })
+
     );
 
 });
